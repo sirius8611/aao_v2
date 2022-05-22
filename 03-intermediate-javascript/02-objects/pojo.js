b3a3cdaf11a5896f93274a643122c8798392cde3
Zach Grammon <zachgrammon@gmail.com>
2022-05-21 23:58:46 -0600

    03-02 pojo project

--- 03-intermediate-javascript/02-objects/pojo.js
+++ 03-intermediate-javascript/02-objects/pojo.js
@@ -26,3 +26,12 @@ function catBuilder(name, color, toys) {
 
 // console.log(catBuilder("Nyan", "rainbow", ["poptarts"]));
 // // prints: { name: 'Nyan', color: 'rainbow', toys: [ 'poptarts' ] }
+
+let bigArray = ["apple", 14, 32, 100, { name: "party" }, ["pineapple"]];
+
+// Example:
+// let [firstEl] = bigArray;
+// console.log(firstEl) // prints "apple"
+
+[firstEl, secondEl, thirdEl, fourthEl, { name }, last] = bigArray;
+console.log(last);
