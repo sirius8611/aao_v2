e6cf7e8ca013b1c89a232b577d851815beaed56c
Zach Grammon <zachgrammon@gmail.com>
2022-06-20 19:03:15 -0600

    Add completed context long practice

--- 04-tdd-and-oop/03-context-in-javascript/03-long-practice-context/problems/06-fancy-calculator.js
+++ 04-tdd-and-oop/03-context-in-javascript/03-long-practice-context/problems/06-fancy-calculator.js
@@ -1,10 +1,35 @@
-// Your code here
+const Calculator = require("./02-calculator.js");
+
+class FancyCalculator extends Calculator {
+  setTotal(newTotal) {
+    this.total = newTotal;
+    return this.total;
+  }
+
+  modulo(num) {
+    this.total = this.total % num;
+    return this.total;
+  }
+
+  squared() {
+    return (this.total = this.total * this.total);
+  }
+}
+
+// let fancyCalculator = new FancyCalculator();
+// console.log(fancyCalculator.setTotal(5)); // => 5
+// console.log(fancyCalculator.squared()); // => 25
+// console.log(fancyCalculator.modulo(4)); // => 1
+// console.log(fancyCalculator.total); // => 1
+
+// // can use instance methods on the Calculator class
+// console.log(fancyCalculator.add(9)); // => 10
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = FancyCalculator;
+  module.exports = FancyCalculator;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
--- 04-tdd-and-oop/03-context-in-javascript/03-long-practice-context/problems/08-call-me-later.js
+++ 04-tdd-and-oop/03-context-in-javascript/03-long-practice-context/problems/08-call-me-later.js
@@ -1,11 +1,31 @@
-// Your code here
+class CallCenter {
+  constructor(name) {
+    this.name = name;
+  }
 
+  sayHello() {
+    console.log(`Hello this is ${this.name}`);
+  }
+
+  callMeLater(delay) {
+    setTimeout(() => {
+      this.sayHello();
+    }, delay);
+  }
+}
+// let judy = new CallCenter("Judy");
+// judy.sayHello(); // prints "Hello this is Judy"
+// judy.callMeLater(1000); // waits one second then prints "Hello this is Judy"
+
+// let melan = new CallCenter("Melan");
+// melan.sayHello(); // prints "Hello this is Melan"
+// melan.callMeLater(1000); // waits one second then prints "Hello this is Melan"
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = CallCenter;
+  module.exports = CallCenter;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
