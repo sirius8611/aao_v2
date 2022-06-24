78828dc1957f61d9ce53fcd9ba400f1c2fcc182c
Zach Grammon <zachgrammon@gmail.com>
2022-06-24 11:31:03 -0600

    Add completed es5 classes

--- 04-tdd-and-oop/05-intermediate-tdd/es5-classes/classes/array.js
+++ 04-tdd-and-oop/05-intermediate-tdd/es5-classes/classes/array.js
@@ -1,1 +1,8 @@
-// Your code here
+Array.prototype.isEqual = function (array) {
+  for (let i = 0; i < this.length; i++) {
+    if (array[i] !== this[i]) {
+      return false;
+    }
+  }
+  return true;
+};
