b056ef2a73a43412e3d073e4d9601bc018a4d2b2
Zach Grammon <zachgrammon@gmail.com>
2022-06-23 23:52:37 -0600

    Add implemented get valid moves and get random move methods

--- 04-tdd-and-oop/04-tdd-basics/challenge-projects/ttt-tdd-project/class/computer-player.js
+++ 04-tdd-and-oop/04-tdd-basics/challenge-projects/ttt-tdd-project/class/computer-player.js
@@ -1,27 +1,50 @@
+const { checkWin } = require("./ttt.js");
 
 class ComputerPlayer {
-
   static getValidMoves(grid) {
-    // Your code here
+    let validMoves = [];
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[0].length; col++) {
+        if (grid[row][col] === " ") {
+          validMoves.push({ row: row, col: col });
+        }
+      }
+    }
+    return validMoves;
   }
 
   static randomMove(grid) {
-
-    // Your code here
+    let validMoves = this.getValidMoves(grid);
+    return validMoves[Math.floor(Math.random() * validMoves.length)];
   }
 
   static getWinningMoves(grid, symbol) {
+    let winningMoves = [];
 
-    // Your code here
+    let validMoves = this.getValidMoves(grid);
 
+    validMoves.forEach((move) => {
+      let testGrid = grid.slice();
+      testGrid[move.row][move.col] = symbol;
+      let winningState = checkWin(testGrid);
+      console.log(testGrid);
+      if (winningState) {
+        winningMoves.push(move);
+      }
+      testGrid = {};
+    });
+    return winningMoves;
   }
 
-  static getSmartMove(grid, symbol) {
-
-    // Your code here
-
-  }
-
+  static getSmartMove(grid, symbol) {}
 }
 
 module.exports = ComputerPlayer;
+
+cpu = new ComputerPlayer();
+grid = [
+  ["X", "X", " "],
+  ["O", " ", " "],
+  ["O", " ", " "],
+];
+console.log(ComputerPlayer.getWinningMoves(grid, "X"));
